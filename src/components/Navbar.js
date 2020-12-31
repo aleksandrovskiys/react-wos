c6ea42748e6c92f4ad62c331b49694c1054c68bd
Semyon Aleksandrovskiy <psifisher@gmail.com>
2020-12-31 09:36:54 +0300

    html css template

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="navigation">
+            <a href={"/"}>Profile</a>
+            <a href={"/"}>Messages</a>
+            <a href={"/"}>News</a>
+            <a href={"/"}>Music</a>
+            <a href={"/"}>Settings</a>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img id="content-header-image"
+                 src="https://mk0eclincher1lr2pc48.kinstacdn.com/wp-content/uploads/2020/08/all-integrations-min-1024x201.png"
+                 alt="content logo"/>
+            <div>
+                Avatar + descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
