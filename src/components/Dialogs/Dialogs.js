db60839a7c7f2e559cdeb1fe912d6ba1222e7406
Semyon Aleksandrovskiy <psifisher@gmail.com>
2021-01-02 11:48:08 +0300

    refactoring

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,19 +3,41 @@ import css from "./Dialogs.module.css"
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 
+let dialogsData = [{id: 1, name: 'Andrew'},
+                   {id: 2, name: 'Julia'},
+                   {id: 3, name: 'Alexander'},
+                   {id: 4, name: 'Valera'}];
+
+let messagesData = [
+    {id: 0, text: 'This is first message!'},
+    {id: 1, text: 'Hey!'},
+    {id: 2, text: 'How u doin'},
+    {id: 3, text: 'Yo!'},
+    {id: 4, text: 'Hey!'},
+    {id: 5, text: 'Nice!'},
+    {id: 6, text: 'Lol!'},
+    {id: 7, text: 'adksgjgs!'}
+]
+
+
 const Dialogs = (props) => {
     return (
         <div className={css.dialogs}>
             <div className={css.dialogItems}>
-                <DialogItem id="1" name="Andrew"/>
-                <DialogItem id="2" name="Julia"/>
-                <DialogItem id="3" name="Alexander"/>
-                <DialogItem id="4" name="Valera"/>
+                <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+                <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
+                <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
+                <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
             </div>
             <div className={css.messages}>
-                <Message text="Hey!"/>
-                <Message text="How r u doin"/>
-                <Message text="Yo"/>
+                <Message text={messagesData[0].text}/>
+                <Message text={messagesData[1].text}/>
+                <Message text={messagesData[2].text}/>
+                <Message text={messagesData[3].text}/>
+                <Message text={messagesData[4].text}/>
+                <Message text={messagesData[5].text}/>
+                <Message text={messagesData[6].text}/>
+                <Message text={messagesData[7].text}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -2,6 +2,14 @@ import React from 'react'
 import css from './MyPosts.module.css'
 import Post from "./Post/Post";
 
+let postsData = [
+    {id: 0, body: "This is first post", likes: 21},
+    {id: 1, body: "Another post", likes: 32},
+    {id: 2, body: "God, don't forget it", likes: 64},
+    {id: 3, body: "helee", likes: 1},
+    {id: 4, body: "oh my", likes: 2},
+];
+
 const MyPosts = () => {
     return (
         <div>
@@ -15,8 +23,11 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={css.posts}>
-                <Post body={'This is first post'} likes={'21'}/>
-                <Post body={'It\'s a second post'} likes={'5'}/>
+                <Post body={postsData[0].body} likes={postsData[0].likes}/>
+                <Post body={postsData[1].body} likes={postsData[1].likes}/>
+                <Post body={postsData[2].body} likes={postsData[2].likes}/>
+                <Post body={postsData[3].body} likes={postsData[3].likes}/>
+                <Post body={postsData[4].body} likes={postsData[4].likes}/>
             </div>
         </div>
     )
